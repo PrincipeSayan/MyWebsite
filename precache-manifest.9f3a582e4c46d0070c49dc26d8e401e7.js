00463edfad99d9b5bfc72d9015e449098355cd54
Stefano Fedeli <stefano5.fedeli@mail.polimi.it>
2021-12-26 20:52:54 +0100

    Updates

---
+++ precache-manifest.9f3a582e4c46d0070c49dc26d8e401e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3ddbf2dacdb5aae5c7d32060649ab5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60fa1f4e4a2bee51f31f",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "95e42cc40a6593e6699a",
+    "url": "/portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "60fa1f4e4a2bee51f31f",
+    "url": "/portfolio/static/js/2.d805f410.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.d805f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e42cc40a6593e6699a",
+    "url": "/portfolio/static/js/main.d7910caf.chunk.js"
+  },
+  {
+    "revision": "7b058e7b7ee22994856c",
+    "url": "/portfolio/static/js/runtime-main.30cef35d.js"
+  }
+]);
